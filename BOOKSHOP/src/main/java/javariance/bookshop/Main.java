5700ba0183ab1c63a3e0b2c6a018c7e7a3616fdd
Gallent-Jiraiya <tharindurcg1@gmail.com>
2021-01-17 15:30:57 +0000

    onloadchanges

--- BOOKSHOP/src/main/java/javariance/bookshop/Main.java
+++ BOOKSHOP/src/main/java/javariance/bookshop/Main.java
@@ -25,6 +25,7 @@ public class Main extends javax.swing.JFrame {
     public Main() {
         initComponents();
         setUser();
+        Booktable();
         fillComboAuther();
         OrderChangeStatus();
         
